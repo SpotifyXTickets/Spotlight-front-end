fa0dcab272f3face621c5f659d9a508529e11e4f
stijnveeke <47558683+stijnveeke@users.noreply.github.com>
2023-11-03 01:46:09 +0100

    Fixed some issues with the authtokens and cookies backend was getting spammed.

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,24 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Cookies, useCookies } from "next-client-cookies";
+import { verifyJwtToken } from "@/libs/auth";
+import { JWTPayload } from "jose/dist/types/types";
+
+export const useAuth = () => {
+  const [auth, setAuth] = useState<JWTPayload | null>(null);
+  const cookies = useCookies();
+
+  const getVerifiedToken = async (cookies: Cookies) => {
+    const token = cookies.get("twix_access_token") ?? null;
+    console.log(cookies.get("twix_access_token"));
+    const verifiedToken = await verifyJwtToken(token ?? "");
+
+    setAuth(verifiedToken);
+  };
+
+  useEffect(() => {
+    getVerifiedToken(cookies);
+  }, [cookies]);
+
+  return auth;
+};
