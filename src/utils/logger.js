2605aff5111929ee55e7838abff333054e951e31
Brand Revivers <117737895+FenChango@users.noreply.github.com>
2023-11-17 12:34:18 +0100

    ESLint, Prettier
    Inserted Prettier, Updated Readme.

---
+++ src/utils/logger.js
@@ -0,0 +1,9 @@
+// src/utils/logger.js
+import winston from "winston";
+
+// Set up Winston logger
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
