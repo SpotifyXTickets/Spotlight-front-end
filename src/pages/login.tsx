5b751898b9e18e926797c6b2d508ffbdbd6475fe
I509050 <509050@student.fontys.nl>
2023-12-11 12:56:29 +0100

    Updated Log in page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,31 @@
-import "../styles/pages/_log-in.scss";
+import "../styles/pages/_login.scss";
 
+import Image from "next/image";
+import LoginImage from "../assets/login-image.png";
+import SpotifyIcon from "../assets/spotify-icon.svg";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-
-
 
 export default function Login() {
   return (
     <section>
       <NavBar />
-      <main className="log-in-block">
-        <h1>Best way to find the perfect music event </h1>
-        <p>Welcome to the world of music events that was created for you </p>
-        <button>Continue with Spotify</button>
+      <main className="login">
+        <Image className="login__image" src={LoginImage} alt="Login Image" />
+        <h1 className="login__header">
+          Best way to find the perfect music event{" "}
+        </h1>
+        <p className="login__text">
+          Welcome to the world of music events that was created for you
+        </p>
+        <button className="login__button">
+          <Image
+            className="login__spotify-icon"
+            src={SpotifyIcon}
+            alt="Spotify Icon"
+          />
+          Continue with Spotify
+        </button>
       </main>
       <Footer />
     </section>
